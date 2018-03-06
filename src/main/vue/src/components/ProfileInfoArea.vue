9a8346abee12152ab24af941815e24b5a54efdee
Darina Bebyakina <bebyakina16@gmail.com>
2018-03-07 01:45:18 +0400

    Component - User Information without style

---
+++ src/main/vue/src/components/ProfileInfoArea.vue
@@ -0,0 +1,13 @@
+<template>
+    <UserInfo/>
+</template>
+
+<script>
+import UserInfo from '/components/UserInfo';
+export default {
+    name: 'ProfileInfo',
+    components: {
+        UserInfo: UserInfo
+    }
+};
+</script>
---
+++ src/main/vue/src/components/UserInfo.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-container>
+        <div id="head">
+            <h1>{{ userName }}</h1>
+            <h1>{{ userSurname }}</h1>
+        </div>
+        <div>
+            <h2>Languages:</h2>
+            <li v-for="language in languages" :key="language">
+                {{ language.name }}
+            </li>
+        </div>
+        <div>
+            <h2>Preferred technologies:</h2>
+            <li v-for="technology in technologies" :key="technology">
+                {{ technology.name }}
+            </li>
+        </div>
+        <div>
+            <h2>Place of work:</h2>
+            <p>{{ placeOfWork }}</p>
+        </div>
+        <div>
+            <h2>Job:</h2>
+            <p>{{ job }}</p>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'UserInfo',
+    data() {
+        return {
+            userName: 'Temp',
+            userSurname: 'Temp',
+            languages: [
+                {name: 'Java'},
+                {name: 'PHP'},
+                {name: 'JavaScript'}
+            ],
+            technologies: [
+                {name: 'Spring'},
+                {name: 'Vue'},
+                {name: 'Hibernate'}
+            ],
+            placeOfWork: 'Some company',
+            job: 'Software Engineer'
+        };
+    }
+};
+</script>
