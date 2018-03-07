bc355e1100350cae5c5dbad6f9619ce5ddb13b25
Stenin.Denis <blackkwade@gmail.com>
2018-03-08 01:49:40 +0400

    Fixed:  - fixed reactive var mobile. Added:  - function for trace mobile version.

--- src/main/vue/src/App.vue
+++ src/main/vue/src/App.vue
@@ -3,6 +3,8 @@
         <NavBar @OpenDrawer="switchDrawerVisible"/>
         <Drawer :is-drawer-visible-desktop.sync="isDrawerVisibleDesktop" :is-drawer-visible-mobile="isDrawerVisibleMobile"/>
         <v-content>
+            <div>{{ isDrawerVisibleMobile }}</div>
+            <div>{{ isDrawerVisibleDesktop }}</div>
             <router-view/>
         </v-content>
     </v-app>
@@ -11,6 +13,10 @@
 <script>
 import NavBar from './components/NavBar.vue';
 import Drawer from './components/Drawer';
+
+function isMobile() {
+    return window.innerWidth < 1264;
+}
 export default {
     name: 'App',
     components: {
@@ -20,13 +26,13 @@ export default {
     data() {
         return {
             isDrawerVisibleDesktop: true,
-            isDrawerVisibleMobile: false,
-            mobile: window.innerWidth <= 1264
+            isDrawerVisibleMobile: false
         };
     },
     methods: {
         switchDrawerVisible() {
-            if (this.mobile === false) {
+            let mobileVersion = isMobile();
+            if (!mobileVersion) {
                 this.isDrawerVisibleMobile = false;
                 this.isDrawerVisibleDesktop = !this.isDrawerVisibleDesktop;
             } else {
