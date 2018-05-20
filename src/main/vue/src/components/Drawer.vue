1c31eed18e2fe76792155fb3bd6c59e23269de3e
Stenin.Denis <blackkwade@gmail.com>
2018-05-20 23:02:21 +0400

    Remove check on organisation name in string.

--- src/main/vue/src/components/Drawer.vue
+++ src/main/vue/src/components/Drawer.vue
@@ -16,7 +16,7 @@
                     </v-list-tile-avatar>
                     <v-list-tile-content>
                         <v-list-tile-title>{{ userData.realName }}</v-list-tile-title>
-                        <v-list-tile-sub-title v-if="userData.organization && userData.organization !== 'null'">
+                        <v-list-tile-sub-title v-if="userData.organization">
                             {{ userData.organization }}
                         </v-list-tile-sub-title>
                     </v-list-tile-content>
