1720c62b7329d9bd25257375d0fff12e8f2ffab6
Stenin.Denis <blackkwade@gmail.com>
2018-05-22 19:45:30 +0400

    Added: - Search in friend name Improved:  - card design

--- src/main/vue/src/components/FriendsList.vue
+++ src/main/vue/src/components/FriendsList.vue
@@ -1,35 +1,50 @@
 <template>
     <v-container>
         <v-card>
-            <v-text-field class="px-5"/>
+            <v-layout row wrap class="ml-3">
+                <v-icon x-large class="search_icon">search</v-icon>
+                <v-flex d-flex class="ml-3 mr-5">
+                    <v-text-field v-model="nameFilter" label="Начните вводить имя друга!"/>
+                </v-flex>
+            </v-layout>
         </v-card>
-        <v-card class="mt-2" v-for="friend in computedFriends" :key="friend.id">
+        <v-card v-for="friend in filterFriends(computedFriends, nameFilter)" :key="friend.id" class="mt-1">
             <v-container>
-                <v-layout row>
-                    <v-flex xs2>
-                        <v-badge right overlap bottom color="transparent">
-                            <v-avatar slot="badge" size="15" v-if="friend.online">
-                                <img src="../assets/online-indicator.png">
-                            </v-avatar>
-                            <v-avatar size="75">
-                                <img :src="friend.avatar">
-                            </v-avatar>
-                        </v-badge>
-                    </v-flex>
-                    <v-flex xs8>
-                        <v-card-title class="body-2">{{ friend.name }}</v-card-title>
-                    </v-flex>
-                    <v-flex xs5>
-                        <v-card-actions>
-                            <v-btn flat color="primary">написать</v-btn>
-                            <v-btn flat color="error">удалить</v-btn>
-                        </v-card-actions>
-                    </v-flex>
-                    <v-flex>
-                        <v-btn flat icon>
-                            <v-icon click="">settings</v-icon>
-                        </v-btn>
-                    </v-flex>
+                <v-layout row wrap>
+                    <v-badge right overlap bottom color="transparent">
+                        <v-avatar slot="badge" size="15" v-if="friend.online">
+                            <img src="../assets/online-indicator.png">
+                        </v-avatar>
+                        <v-avatar size="75">
+                            <img :src="friend.avatar">
+                        </v-avatar>
+                    </v-badge>
+                    <v-layout justify-space-between>
+                        <v-flex xs4 inline-flex>
+                            <v-card-title class="body-2">{{ friend.name }}</v-card-title>
+                        </v-flex>
+                        <v-flex d-flex xs1>
+                            <v-card-actions>
+                                <v-menu open-on-hover offset-y>
+                                    <v-icon large slot="activator">more_horiz</v-icon>
+                                    <v-card>
+                                        <v-list>
+                                            <v-list-tile>
+                                                <v-btn flat color="primary">
+                                                    <v-icon class="pr-2">message</v-icon>
+                                                    написать
+                                                </v-btn>
+                                                <v-btn flat color="error">
+                                                    <v-icon class="pr-2">delete</v-icon>
+                                                    удалить
+                                                </v-btn>
+                                            </v-list-tile>
+                                        </v-list>
+                                    </v-card>
+                                </v-menu>
+                            </v-card-actions>
+                        </v-flex>
+                    </v-layout>
                 </v-layout>
             </v-container>
         </v-card>
@@ -37,12 +52,15 @@
 </template>
 
 <script>
+/* eslint-disable eqeqeq */
+
 export default {
     name: 'Friend',
     data() {
         return {
-            id: 1,
-            friends: this.$store.state.friends.friends
+            friends: this.$store.state.friends.friends,
+            nameFilter: '',
+            searchVisible: false
         };
     },
     computed: {
@@ -60,10 +78,20 @@ export default {
     },
     mounted() {
         this.$store.dispatch('friends/updateFriends');
+    },
+    methods: {
+        filterFriends(computedFriends, nameFilter) {
+            return computedFriends.filter(function (friend) {
+                if (nameFilter) {
+                    return friend.name.toLowerCase().includes(nameFilter);
+                } else {
+                    return friend;
+                }
+            });
+        }
     }
 };
 </script>
 
 <style scoped>
-
 </style>
