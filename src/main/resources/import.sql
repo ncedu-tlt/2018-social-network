b8b6280fb1651f7b52c595df00ada7f1de02ff9d
Tadoma <midinq@gmail.com>
2018-06-17 15:40:26 +0400

    Fixed for review profile

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 -- Hibernate will execute the SQL statements read from this file after the creation of the database schema.
-INSERT INTO users VALUES (2000, 'https://octodex.github.com/images/electrocat.png', 'zorin', 'Andrey Zorin');
-INSERT INTO users VALUES (3000, 'https://octodex.github.com/images/electrocat.png', 'sotn', 'Alexandra Sotnikova');
+INSERT INTO users VALUES (2000, 'https://octodex.github.com/images/electrocat.png', 'zorin', 'Andrey Zorin', null);
+INSERT INTO users VALUES (3000, 'https://octodex.github.com/images/electrocat.png', 'sotn', 'Alexandra Sotnikova', null);
